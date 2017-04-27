7e07a25729be52166f7d1eb6fa9292d15a95d622
Mariana <mariana@wifi-interno.fe.up.pt>
2017-04-27 12:00:55 +0100

    adicionadas funções das classes Line e Driver

--- src/Driver.h
+++ src/Driver.h
@@ -13,12 +13,13 @@ class Driver{
  private:
   unsigned int id;
   string name;
-  unsigned int maxHours;        // maximum duration of a shift
-  unsigned int maxWeekWorkingTime;  // maximum number of hours of work in a week
-  unsigned int minRestTime;     // minimum number of rest hours between shifts
+  unsigned int max_shift;        // maximum duration of a shift
+  unsigned int max_week;  // maximum number of hours of work in a week
+  unsigned int min_rest;     // minimum number of rest hours between shifts
   vector<Shift> shifts;         // assigned shifts
  public:
   Driver(string textLine);
+  Driver(int id, string name, unsigned int max_shift, unsigned int max_week, unsigned int min_rest, vector<Shift> shifts);
   // get methods
   unsigned int getId() const;
   string getName() const;
@@ -28,5 +29,5 @@ class Driver{
   vector<Shift> getShifts() const;
 
   // outher methods
-
+  void showDriver(Driver);
 };
--- src/Line.h
+++ src/Line.h
@@ -10,16 +10,21 @@ using namespace std;
 
 class Line{
  private:
-  unsigned int id;
+  unsigned int id_num;
+  unsigned int freq;
   vector<string> busStopList;
   vector<int> timesList;
  public:
   Line(string textLine);
   // metodos get
   unsigned int getId() const;
+  unsigned int getFreq() const;
   vector<string> getBusStops() const;
   vector<int> getTimings() const;
   // set methods
   // other methods
-
+  void showLine(Line) const;
+  void showSchedule(Line) const;
+  int getTimeStops(Line, string stop1, string stop2);
+  void showScheduleStops(Line);
 };
