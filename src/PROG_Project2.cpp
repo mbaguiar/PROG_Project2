cecf9f29d94b88260d7348881f471e5ba103a12b
Mariana <mariana@wifi-interno.fe.up.pt>
2017-04-28 14:41:01 +0100

    no message

--- src/PROG_Project2.cpp
+++ src/PROG_Project2.cpp
@@ -7,8 +7,11 @@
 //============================================================================
 
 #include <iostream>
+#include "Application.h"
 using namespace std;
 
 int main() {
-
+	Application app;
+	app.setupMenu();
+	app.displayMenu();
 }
