cef5c2674c8ae1c5819c0554a54bca8f91e1acd8
Mariana Aguiar <marianabarbosaaguiar@gmail.com>
2017-05-11 13:48:40 +0100

    sets e gets de bus e shift

--- src/Bus.h
+++ src/Bus.h
@@ -16,15 +16,18 @@ class Bus{
   unsigned int lineId;
   vector<Shift> schedule;
  public:
-  Bus(unsigned int id, unsigned int driver, unsigned int line);
+  Bus(unsigned int orderInLine, unsigned int driverId, unsigned int lineId);
   // get methods
   unsigned int getBusOrderInLine() const;
   unsigned int getDriverId() const;
   unsigned int getLineId() const;
   vector<Shift> getSchedule() const;
   // set methods
-    unsigned int setDriverId() const;
-    unsigned int setLineId() const;
+   void setOrderInLine(unsigned int orderInLine);
+   void setLineId(unsigned int lineId);
+   void setDriverId(unsigned int driverId);
+   void setSchedule(vector<Shift> schedule);
+   void addShift(Shift shift);
   // other methods
 
 };
